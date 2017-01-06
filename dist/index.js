33fc74b82f2bc82e1c9911a3007840f61abbde85
kicoamorim@gmail.com <kicoamorim@gmail.com>
2017-01-06 12:41:36 -0200

    usando babel

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
-const title = React.createElement(
-	'h1',
-	{id:'title',className:'header'},
-	'React JS Essentials'
-	);
-ReactDOM.render(
-	title,
+const { render } = ReactDOM
+
+render(
+	<h1 id="title" className="header"
+	style={{backgroundColor: 'green',color:'white',fontFamily:'verdana'}}>
+		LuisAmorim
+	</h1>,
 	document.getElementById('react-container')
 	);
