0e0acba1088ec8efbaafbcbc871f38ad86191280
kicoamorim@gmail.com <kicoamorim@gmail.com>
2017-01-06 12:30:38 -0200

    pure react

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+const title = React.createElement(
+	'h1',
+	{id:'title',className:'header'},
+	'React JS Essentials'
+	);
+ReactDOM.render(
+	title,
+	document.getElementById('react-container')
+	);
