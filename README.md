efcf6771fdc49a82766052b7bf247f144bd373b4
kicoamorim@gmail.com <kicoamorim@gmail.com>
2017-01-06 12:57:47 -0200

    config babel cli

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # reactjs-essentials
 
 npm httpster -d ./dir -p 3000
+
+npm babel-preset-latest install babel-preset-react babel-preset-stage-0
+
+comando para gerar o arquivo bundle.js
+
+babel ./src/index.js --out-file ./dist/bundle.js
+
+
+
---
+++ dist/bundle.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _ReactDOM = ReactDOM,
+    render = _ReactDOM.render;
+
+
+render(React.createElement(
+	"h1",
+	{ id: "title", className: "header",
+		style: { backgroundColor: 'green', color: 'white', fontFamily: 'verdana' } },
+	"LuisAmorim"
+), document.getElementById('react-container'));
